884a085c58af4d1e4b6d2065d4aaa3ff76cb3287
Stantoncomet <olivercraftmoore@gmail.com>
2024-10-15 20:49:41 -0700

    First Test!
    jsconfig to select which folders to use. server might already have one, but its here now just incase cause its required
    
    right-click a stick for a surpise!

--- kubejs/server_scripts/example.js
+++ kubejs/server_scripts/example.js
@@ -2,5 +2,10 @@
 
 // Visit the wiki for more info - https://kubejs.com/
 
-console.info('Hello, World! (Loaded server scripts)')
+console.info("Hello, World! (Loaded server scripts)");
 
+ItemEvents.rightClicked('minecraft:stick', e => {
+    e.player.give("minecraft:charcoal");
+    e.level.tell(Component.aqua(`Someone named ${e.player.username} really just right-clicked a stick`));
+    e.level.spawnParticles("minecraft:wax_on", false, e.player.x, e.player.y, e.player.z, .1, .1, .1, 40, 10);
+})
