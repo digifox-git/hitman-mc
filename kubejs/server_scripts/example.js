afa3306e28adb0e7234048f117384b0720dcd88e
Stantoncomet <olivercraftmoore@gmail.com>
2024-10-19 12:04:17 -0700

    start game

--- kubejs/server_scripts/example.js
+++ kubejs/server_scripts/example.js
@@ -6,6 +6,8 @@ console.info("Hello, World! (Loaded server scripts)");
 
 ItemEvents.rightClicked('minecraft:stick', e => {
     e.player.give("minecraft:charcoal");
-    e.level.tell(Component.aqua(`Someone named ${e.player.username} is super cool!!`));
+    //e.level.tell(Component.aqua(`Someone named ${e.player.username} is super cool!!`));
     e.level.spawnParticles("minecraft:wax_on", false, e.player.x, e.player.y, e.player.z, .1, .1, .1, 40, 10);
 })
+
+
