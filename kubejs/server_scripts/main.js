664c95941abc6a9dbeb4bc866db927d71258c2ab
Stantoncomet <olivercraftmoore@gmail.com>
2024-10-19 12:57:22 -0700

    giard tp to spawn

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -13,18 +13,18 @@ function startGame(s) {
 ItemEvents.entityInteracted("minecraft:interaction", e => {
     e.server.tell(e.entity.tags)
     e.level.spawnParticles("minecraft:wax_on", false, e.target.x, e.target.y, e.target.z, .1, .1, .1, 40, 10);
-    startGame(e.server);
-    global.player_c = e.server.playerCount;
+    // startGame(e.server);
+    // global.player_c = e.server.playerCount;
     // Get all players with right tags tag
     let guards = e.server.players.filter(p => p.tags.contains("guard"));
     //global.guards = selectE(e, "guard");
-    global.hitman = selectE(e, "hitman");
-    global.target = selectE(e, "target");
+    // global.hitman = selectE(e, "hitman");
+    // global.target = selectE(e, "target");
     // Get spawns
     let g_spawn = selectE(e, "g_spawn")[0].pos;
-    global.h_spawn = selectE(e, "h_spawn")[0].pos;
-    global.g_respawn = selectE(e, "g_respawn")[0].pos;
-    global.h_respawn = selectE(e, "h_respawn")[0].pos; // Hitman doesn't respawn, but this is where they're put while target is hiding
+    // global.h_spawn = selectE(e, "h_spawn")[0].pos;
+    // global.g_respawn = selectE(e, "g_respawn")[0].pos;
+    // global.h_respawn = selectE(e, "h_respawn")[0].pos; // Hitman doesn't respawn, but this is where they're put while target is hiding
     //Teleport players to proper places
     guards.forEach(g => g.teleportTo(g_spawn));
     //e.server.tell(guards);
