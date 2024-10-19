af6857e0d72445898aa7e70226bfeefcabc2ea2d
Stantoncomet <olivercraftmoore@gmail.com>
2024-10-19 13:00:18 -0700

    global attempt

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -16,7 +16,7 @@ ItemEvents.entityInteracted("minecraft:interaction", e => {
     // startGame(e.server);
     // global.player_c = e.server.playerCount;
     // Get all players with right tags tag
-    let guards = e.server.players.filter(p => p.tags.contains("guard"));
+    global.guards = e.server.players.filter(p => p.tags.contains("guard"));
     //global.guards = selectE(e, "guard");
     // global.hitman = selectE(e, "hitman");
     // global.target = selectE(e, "target");
@@ -27,6 +27,6 @@ ItemEvents.entityInteracted("minecraft:interaction", e => {
     // global.h_respawn = selectE(e, "h_respawn")[0].pos; // Hitman doesn't respawn, but this is where they're put while target is hiding
     //Teleport players to proper places
     //guards.forEach(g => g.teleportTo(g_spawn));
-    e.server.tell(guards);
+    e.server.tell(global.guards);
     e.server.tell(g_spawn);
 })
