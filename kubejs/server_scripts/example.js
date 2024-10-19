87ca9556aa14d983f7f57bd327430786d6501ba8
Stantoncomet <olivercraftmoore@gmail.com>
2024-10-18 21:51:21 -0700

    stic kupdate

--- kubejs/server_scripts/example.js
+++ kubejs/server_scripts/example.js
@@ -6,6 +6,6 @@ console.info("Hello, World! (Loaded server scripts)");
 
 ItemEvents.rightClicked('minecraft:stick', e => {
     e.player.give("minecraft:charcoal");
-    e.level.tell(Component.aqua(`Someone named ${e.player.username} really just right-clicked a stick. Can you believe it?`));
+    e.level.tell(Component.aqua(`Someone named ${e.player.username} is super cool!!`));
     e.level.spawnParticles("minecraft:wax_on", false, e.player.x, e.player.y, e.player.z, .1, .1, .1, 40, 10);
 })
