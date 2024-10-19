4f37e56c96a1a7f0a5a3166b660d196f4fcc061a
Stantoncomet <olivercraftmoore@gmail.com>
2024-10-19 12:54:59 -0700

    added wahat
    another test

--- kubejs/server_scripts/wahat.js
+++ kubejs/server_scripts/wahat.js
@@ -1,4 +1,4 @@
-priority: 10
+// priority: 0
 
 // Visit the wiki for more info - https://kubejs.com/
 
@@ -6,8 +6,8 @@ console.info("Hello, World! (Loaded server scripts)");
 
 ItemEvents.rightClicked('minecraft:stick', e => {
     e.player.give("minecraft:charcoal");
-    //e.level.tell(Component.aqua(`Someone named ${e.player.username} is super cool!!`));
+    e.level.tell(Component.aqua(`Someone named ${e.player.username} really just right-clicked a stick`));
     e.level.spawnParticles("minecraft:wax_on", false, e.player.x, e.player.y, e.player.z, .1, .1, .1, 40, 10);
+    e.player.swing();
+    
 })
-
-
