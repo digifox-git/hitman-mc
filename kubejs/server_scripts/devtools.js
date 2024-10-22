34c86f551f2109056ea649f1f90a1051355c419f
Stantoncomet <olivercraftmoore@gmail.com>
2024-10-21 22:32:57 -0700

    s

--- kubejs/server_scripts/devtools.js
+++ kubejs/server_scripts/devtools.js
@@ -13,28 +13,28 @@ ItemEvents.rightClicked("minecraft:nether_star", e => {
     }
     // Load Kits
     else {
-        e.player.tell(kits.guard.inv[0].id);
-        e.player.giveInHand(kits.guard.inv[1].id);
-        e.player.setOffHandItem(Item.of(kits.guard.armor[2].id, kits.guard.armor[2].count, kits.guard.armor[2].nbt));
-        // Items
-        //if (!Array.isArray(kits.guard.inv)) return;
-        kits.guard.inv.forEach(item => {
-            e.player.tell(item)
-            e.player.giveInHand(item.id);
-        });
+        // e.player.tell(kits.guard.inv[0].id);
+        // e.player.giveInHand(kits.guard.inv[1].id);
+        e.player.giveInHand(Item.of(kits.guard.armor[2].id, kits.guard.armor[2].count, kits.guard.armor[2].nbt));
+        // // Items
+        // //if (!Array.isArray(kits.guard.inv)) return;
+        // kits.guard.inv.forEach(item => {
+        //     e.player.tell(item)
+        //     e.player.giveInHand(item.id);
+        // });
         
-        // Offhand
-        let oh = kits.guard.offhand;
-        //e.player.setOffHandItem(Item.of(oh.id, oh.count));
-        e.player.tell("Inv loaded");
+        // // Offhand
+        // let oh = kits.guard.offhand;
+        // //e.player.setOffHandItem(Item.of(oh.id, oh.count));
+        // e.player.tell("Inv loaded");
         
-        // Armor
-        if (!Array.isArray(kits.guard.armor)) return;
-        let armor = kits.guard.armor;
-        e.player.setFeetArmorItem(Item.of(armor[0].id, armor[0].count, armor[0].nbt));
-        e.player.setLegsArmorItem(Item.of(armor[1].id, armor[1].count, armor[1].nbt));
-        e.player.setChestArmorItem(Item.of(armor[2].id, armor[2].count, armor[2].nbt));
-        e.player.setHeadArmorItem(Item.of(armor[3].id, armor[3].count, armor[3].nbt));
-        e.player.tell("Armor loaded");
+        // // Armor
+        // if (!Array.isArray(kits.guard.armor)) return;
+        // let armor = kits.guard.armor;
+        // e.player.setFeetArmorItem(Item.of(armor[0].id, armor[0].count, armor[0].nbt));
+        // e.player.setLegsArmorItem(Item.of(armor[1].id, armor[1].count, armor[1].nbt));
+        // e.player.setChestArmorItem(Item.of(armor[2].id, armor[2].count, armor[2].nbt));
+        // e.player.setHeadArmorItem(Item.of(armor[3].id, armor[3].count, armor[3].nbt));
+        // e.player.tell("Armor loaded");
     }
 })
