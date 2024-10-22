5cf339c37e79e1fd91aac2e9e635add400b21ae7
Stantoncomet <olivercraftmoore@gmail.com>
2024-10-21 21:54:27 -0700

    load kits cont

--- kubejs/server_scripts/devtools.js
+++ kubejs/server_scripts/devtools.js
@@ -14,5 +14,20 @@ ItemEvents.rightClicked("minecraft:nether_star", e => {
     // Load Kits
     else {
         e.server.tell(kits);
+
+        // Items
+        if (!Array.isArray(kits.guard.inv)) return;
+        kits.guard.inv.forEach(item => {
+            e.player.giveInHand(Item.of(item.id, item.count, item.nbt));
+        });
+        // Offhand
+        let oh = kits.guard.offhand;
+        // Armor
+        if (!Array.isArray(kits.guard.armor)) return;
+        let armor = kits.guard.armor;
+        e.player.setFeetArmorItem(Item.of(armor[0].id, armor[0].count, armor[0].nbt));
+        e.player.setLegsArmorItem(Item.of(armor[1].id, armor[1].count, armor[1].nbt));
+        e.player.setChestArmorItem(Item.of(armor[2].id, armor[2].count, armor[2].nbt));
+        e.player.setHeadArmorItem(Item.of(armor[3].id, armor[3].count, armor[3].nbt));
     }
 })
--- kubejs/server_scripts/helpers.js
+++ kubejs/server_scripts/helpers.js
@@ -3,3 +3,6 @@ priority: 0
 function selectE(e, tag) {
     return e.server.entities.filter(i => i.tags.contains(tag));
 }
+function loadKit(player, kit) {
+    //JsonIO.readJson()
+}
