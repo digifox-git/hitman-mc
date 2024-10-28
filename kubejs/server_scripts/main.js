16131a25fb48238356f202bbb15d1482c5ce7468
ThatCrazyManInASuit <57328239+ThatCrazyManInASuit@users.noreply.github.com>
2024-10-27 23:00:03 -0500

    Update main.js

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -39,7 +39,7 @@ function startGame(server) {
     global.guards.forEach(guard => {
         guard.teleportTo(global.map.gSpawn.x, global.map.gSpawn.y, global.map.gSpawn.z);
         loadKit(guard, "guard", true);
-        guard.giveInHand(Item.of("minecraft:villager_spawn_egg", 1, '{EntityTag:{Tags:["target"], NoAI:1b},CanPlaceOn:["minecraft:green_concrete"}'));
+        guard.giveInHand(Item.of("minecraft:villager_spawn_egg", 1, '{EntityTag:{Tags:["target"], NoAI:1b}'));
         guard.paint({
             respawn_time: {
                 type: 'text',
@@ -89,7 +89,7 @@ function endGame(server) {
     } else {
         server.tell("guards won");
     }
-    
+    server.runCommandSilent(`tp @a -10000 -47 -10000`)
 }
 
 /**
