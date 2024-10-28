ff78a37ed52796bc0f36686b358e61e35224b172
ThatCrazyManInASuit <57328239+ThatCrazyManInASuit@users.noreply.github.com>
2024-10-27 22:15:54 -0500

    Update main.js

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -1,6 +1,6 @@
 // priority: 1;
 
-/*class Location {
+class Location {
     constructor(spawnSelection, hitmanSpawns, guardSpawns) {
       this.spawnSelection = spawnSelection;
       this.hitmanSpawns = hitmanSpawns;
@@ -12,7 +12,7 @@ const theWoods = new Location(false, [176,-60,209], [-97.5,-57,134]);
 const freeformTraining = new Location(false,[101,-55,20],[58,-56,26]);
 
 const mapOptions = [theWoods, freeformTraining]
-*/
+
 let hpoints, gpoints;
 let targetAlive
 // Utility function to select entities by tag
