06b1e287c0a9c997ab73acfb21c42c5ac01c1032
ThatCrazyManInASuit <57328239+ThatCrazyManInASuit@users.noreply.github.com>
2024-11-17 14:49:13 -0600

    Update main.js

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -8,10 +8,10 @@ function selectE(server, tag) {
 }
 
 BlockEvents.rightClicked("black_glazed_terracotta", e => {
-    selectE(server, "hitman").getTags().add('guard')
-    selectE(server, "hitman").getTags().remove('hitman')
+    // selectE(server, "hitman").getTags().add('guard')
+    // selectE(server, "hitman").getTags().remove('hitman')
     e.player.getTags().remove('guard')
-    e.player.getTags().add('hitman')
+    // e.player.getTags().add('hitman')
     e.server.tell(`${e.player.username} is now the Hitman!`)
 });
 
