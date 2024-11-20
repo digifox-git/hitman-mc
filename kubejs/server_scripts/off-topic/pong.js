c94027ba0288b4b1b82468f87434ca02522af0da
Stantoncomet <olivercraftmoore@gmail.com>
2024-11-19 20:10:29 -0800

    pong6.4

--- kubejs/server_scripts/off-topic/pong.js
+++ kubejs/server_scripts/off-topic/pong.js
@@ -110,7 +110,7 @@ ItemEvents.rightClicked("minecraft:gold_nugget", e => {
 })
 
 ItemEvents.rightClicked("minecraft:iron_nugget", e => {
-    e.player.tell(e.server.getScheduledEvents().events);
+    e.player.tell(e.server.getScheduledEvents().events.first);
 
     
     gameLoop(e.level);
