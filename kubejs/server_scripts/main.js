147eb567c40259eaab5497bd0e62c6bc5e8ad05d
ThatCrazyManInASuit <57328239+ThatCrazyManInASuit@users.noreply.github.com>
2024-11-22 22:59:15 -0600

    Update main.js

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -153,7 +153,7 @@ function respawnGuard(guard) {
     guard.teleportTo(global.map.gSpawn.x, global.map.gSpawn.y, global.map.gSpawn.z);
     guard.persistentData.respawnTime = 120;
     guard.paint({ respawn_time: { visible: true } });
-    loadKit(guard, "guard", true)
+    global.guards.forEach(guard => loadKit(guard, "guard", true));
 }
 
 /**
