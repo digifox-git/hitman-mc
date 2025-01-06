c881ebc15cafe73b000758303d9f498029ba2ee0
digifox <digifox08@gmail.com>
2025-01-06 10:12:51 -0600

    Update main.js

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -25,7 +25,7 @@ BlockEvents.rightClicked('minecraft:purple_concrete_powder', e => {
     console.log(global.spawnposX)
     console.log(global.spawnposY)
     console.log(global.spawnposZ)
-    console.log(e)
+    console.log(e.player.username)
 })
 
 BlockEvents.rightClicked("black_glazed_terracotta", e => {
