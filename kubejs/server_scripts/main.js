4ae539222514260fa2818e0ec1ed16df5a3811fc
digifox <digifox08@gmail.com>
2025-01-05 18:19:24 -0600

    Update main.js

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -168,7 +168,6 @@ EntityEvents.death(e => {
         e.server.tell("Guard down!");
         e.server.runCommandSilent(`playsound minecraft:entity.bat.death master @a ~ ~ ~ 0.25 0.6 1`)
         respawnGuard(e.entity);
-        e.entity.teleportTo(global.guardPosX, global.guardPosY, global.guardPosZ);
     }
 });
 
@@ -194,6 +193,7 @@ PlayerEvents.tick(e => {
         e.player.paint({ respawn_time: { text: `${e.player.persistentData.respawnTime}` } });
         e.entity.setGameMode('spectator')
         e.player.potionEffects.add('minecraft:glowing', 99999, 0, true, true); // "INFINITE isnt defined"
+        e.player.teleportTo(global.guardPosX, global.guardPosY, global.guardPosZ);
     }
 
     // Respawn guard when time reaches zero
