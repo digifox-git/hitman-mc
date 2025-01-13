3647f95aa35185d029a25325fcb8402eb5a83bf3
ThatCrazyManInASuit <57328239+ThatCrazyManInASuit@users.noreply.github.com>
2025-01-12 21:54:28 -0600

    Update main.js

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -286,16 +286,16 @@ BlockEvents.rightClicked("kubejs:monitor", e => {
     if (e.level.getBlock(e.block.x, e.block.y - 2, e.block.z) == 'minecraft:green_concrete') {
         e.server.runCommandSilent('title @a actionbar "Difficulty Selected: Casual"')
         e.server.runCommandSilent('playsound minecraft:block.note_block.bit master @a ~ ~ ~ 1 1 1');
-        global.difficulty = 3
+        global.difficulty = 2
     } 
     if (e.level.getBlock(e.block.x, e.block.y - 2, e.block.z) == 'minecraft:yellow_concrete') {
         e.server.runCommandSilent('title @a actionbar "Difficulty Selected: Professional"')
         e.server.runCommandSilent('playsound minecraft:block.note_block.chime master @a ~ ~ ~ 1 1 1');
-        global.difficulty = 2
+        global.difficulty = 1
     }
     if (e.level.getBlock(e.block.x, e.block.y - 2, e.block.z) == 'minecraft:red_concrete') {
         e.server.runCommandSilent('title @a actionbar "Difficulty Selected: Master"')
         e.server.runCommandSilent('playsound minecraft:block.note_block.harp master @a ~ ~ ~ 1 1 1');
-        global.difficulty = 1
+        global.difficulty = 0
     } 
 });
