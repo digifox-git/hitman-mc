d06200340ef51e7e34e4f46f22efb5932f1a4474
ThatCrazyManInASuit <57328239+ThatCrazyManInASuit@users.noreply.github.com>
2025-01-12 23:24:36 -0600

    Update main.js

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -311,29 +311,17 @@ BlockEvents.rightClicked("kubejs:monitor", e => {
     } 
 });
 
-ServerEvents.commandRegistry(event => {
+ServerEvents.commandRegistry(e => {
     const { commands: Commands, arguments: Arguments } = event
     
-    event.register(Commands.literal('fly') // The name of the command
-      .requires(s => s.hasPermission(2)) // Check if the player has operator privileges
-      .executes(c => fly(c.source.player)) // Toggle flight for the player that ran the command if the `target` argument isn't included
-      .then(Commands.argument('target', Arguments.PLAYER.create(event))
-        .executes(c => fly(Arguments.PLAYER.getResult(c, 'target'))) // Toggle flight for the player included in the `target` argument
-      )
+    e.register(Commands.literal('cancel') // The name of the command
+      .requires(s => s.hasPermission(1)) // Check if the player has operator privileges
+      .executes(() => {
+        e.server.runCommandSilent(`kill @e[tag='target']`)
+        e.server.runCommandSilent(`clear @a`)
+        e.server.runCommandSilent(`tp @a 10000 -42 0`)
+        e.server.runCommandSilent(`kubejs reload server_scripts`)
+      })
     )
     
-    // Helper function
-    let fly = (player) => {
-      console.log(player)
-      if (player.abilities.mayfly) {
-        player.abilities.mayfly = false
-        player.abilities.flying = false
-        player.displayClientMessage(Component.gold('Flying: ').append(Component.red('disabled')), true)
-      } else {
-        player.abilities.mayfly = true
-        player.displayClientMessage(Component.gold('Flying: ').append(Component.green('enabled')), true)
-      }
-      player.onUpdateAbilities()
-      return 1
-    }
   })
