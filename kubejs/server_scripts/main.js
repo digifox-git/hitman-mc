fa0e4efc56ae886d6d6ec891bbab17a22253cb73
digifox <digifox08@gmail.com>
2025-05-15 23:11:59 -0500

    Update main.js

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -362,7 +362,7 @@ BlockEvents.rightClicked("kubejs:monitor", e => {
 
 
 
-BlockEvents.rightClicked(`${/minecraft:.*_slab$/g}`, e => {
+BlockEvents.rightClicked((/minecraft:.*_slab$/g).test, e => {
     if (e.getHand() == "off_hand") return
     if (e.player.username == "digifox_"){
             e.server.tell(`${e.block}`)
