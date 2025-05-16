55d1b95befea6ee14b53688afb043a1b2437c3a3
digifox <digifox08@gmail.com>
2025-05-16 18:52:44 -0500

    Update main.js

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -364,7 +364,7 @@ BlockEvents.rightClicked("kubejs:monitor", e => {
 Ingredient.of('#minecraft:slabs').itemIds.forEach(x => {
     BlockEvents.rightClicked(x, e => {
         if (e.getHand() == "off_hand") return
-        e.server.tell(`${alert(show_all_methods(e.player))}`)
+        e.server.tell(`${Object.getOwnPropertyNames(e.player)}`)
     })
 })
 
