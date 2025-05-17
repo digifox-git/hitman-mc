0cc43d51881b490196b3eaa0542a4ba8041fd194
digifox <digifox08@gmail.com>
2025-05-16 21:07:32 -0500

    Update main.js

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -362,7 +362,6 @@ BlockEvents.rightClicked("kubejs:monitor", e => {
 const Pose = Java.loadClass('net.minecraft.world.entity.Pose')
 
 Ingredient.of('#minecraft:slabs').itemIds.forEach(e => {
-    if (e.getHand() == "off_hand") return
     if (e.level.getBlock(e.block.x, e.block.y - 3, e.block.z) == 'minecraft:red_glazed_terracotta') {
     if (e.block.getPlayersInRadius(16) = true) {
         e.player.setPose(Pose.SWIMMING);
