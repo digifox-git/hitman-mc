44b225ee25a3c818333a0e343a3e5e7f98c7560b
digifox <digifox08@gmail.com>
2025-05-17 10:16:49 -0500

    Update main.js

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -230,7 +230,7 @@ PlayerEvents.tick(e => {
 
     if (e.player.block.down.id == "minecraft:brown_glazed_terracotta") {
         // e.player.setPose(Pose.SWIMMING);
-        e.server.tell(`${e.player.distanceToEntitySqr(window)}`)
+        e.server.tell(`${e.player.distanceToSqr(window)}`)
     }
 })
 
@@ -366,10 +366,6 @@ BlockEvents.rightClicked("kubejs:monitor", e => {
 
     if (e.level.getBlock(e.block.x, e.block.y - 2, e.block.z) == 'minecraft:lapis_block') {
         e.server.runCommandSilent(`openguiscreen teamsettings ${e.player.username}`)
-    }
-
-    if (e.level.getBlock(e.block.x, e.block.y - 2, e.block.z) == 'minecraft:brown_glazed_terracotta') {
-        e.server.tell(`${e.player.distanceToEntitySqr(e.entity.tags.contains('window'))}`)
     }
 });
 
