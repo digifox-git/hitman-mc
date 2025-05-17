b4915635f4eebcc4fedc649f6ce4ccc491606f9e
digifox <digifox08@gmail.com>
2025-05-16 20:52:27 -0500

    Update main.js

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -366,7 +366,7 @@ Ingredient.of('#minecraft:slabs').itemIds.forEach(x => {
     if (e.getHand() == "off_hand") return
     if (e.level.getBlock(e.block.x, e.block.y - 3, e.block.z) == 'minecraft:red_glazed_terracotta') {
     e.player.setPose(Pose.SWIMMING);
-    e.server.tell(e.block.distanceToSqr(e.player))
+    e.server.tell(Object.keys(e.block))
     }
     })
 })
