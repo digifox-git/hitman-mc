7c241d4f1f59d91969ad7b3adb6ebfbfafa002ec
digifox <digifox08@gmail.com>
2025-05-18 11:17:23 -0500

    Update main.js

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -236,12 +236,19 @@ EntityEvents.spawned("minecraft:villager", e => {
 
 PlayerEvents.tick(e => {
         const Pose = Java.loadClass('net.minecraft.world.entity.Pose')
+        const crawlMode = false
 
         let distance = Math.hypot(e.player.x - global.windowPos[0], e.player.y - global.windowPos[1], e.player.z - global.windowPos[2])
     
-        if (distance < 3.5 && e.player.isCrouching()) {
-            e.player.setPose(Pose.SWIMMING);
+        if (distance < 3 && e.player.isCrouching()) {
             e.player.potionEffects.add('minecraft:speed', 1, 2, false, false)
+            crawlMode = true
+        } else {
+            crawlMode = false
+        }
+
+        if (crawlMode = true) {
+            e.player.setPose(Pose.SWIMMING);
         }
 })
 
