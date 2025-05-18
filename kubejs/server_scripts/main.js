bb415aab4f68f81e63fcd3dbb735e4cb271d596f
digifox <digifox08@gmail.com>
2025-05-18 10:36:18 -0500

    Update main.js

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -226,11 +226,9 @@ function respawnGuard(guard) {
 PlayerEvents.tick(e => {
         // const Pose = Java.loadClass('net.minecraft.world.entity.Pose')
 
-    let window = e.entity.tags.contains('window')
-
     if (e.player.block.down.id == "minecraft:brown_glazed_terracotta") {
         // e.player.setPose(Pose.SWIMMING);
-        e.server.tell(`${e.player.distanceToEntity(window)}`)
+        e.server.tell(`${e.player.distanceToEntity(e.entity.tags.contains('window'))}`)
     }
 })
 
