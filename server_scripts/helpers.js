bb0e7365be9f626db927ce6a4ec23024c8413e32
ThatCrazyManInASuit <skylarchung@gmail.com>
2026-08-13 03:21:48 -0500

    Update helpers.js

--- server_scripts/helpers.js
+++ server_scripts/helpers.js
@@ -105,10 +105,10 @@ function loadKit(server, player, kit, clear_inv) {
     server.tell(`[DEBUG] Found kit ${kit}!`);
     if (clear_inv) player.inventory.clear();
     // Items
-    if (!Array.isArray(kits[kit].inv)) return false;
-    server.tell(`[DEBUG] ${kit} indeed has an inventory array!`);
+    // if (!Array.isArray(kits[kit].inv)) return false;
+    // server.tell(`[DEBUG] ${kit} indeed has an inventory array!`);
     kits[kit].inv.forEach(item => {
-
+        server.tell("Here is your gun!");
         server.runCommandSilent(`give ${player.username} ${item.id}${item.nbt} ${item.count}`)
         // player.give(`${item.count}x ${item.id} ${item.nbt}`);
     });
