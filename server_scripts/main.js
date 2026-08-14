e23ebce4dd48a8531a4bbf991c0fb552c7b077b3
digifox <digifox08@gmail.com>
2026-08-14 13:55:05 -0500

    Update main.js

--- server_scripts/main.js
+++ server_scripts/main.js
@@ -459,8 +459,11 @@ PlayerEvents.tick(e => {
     }
 
     if (isVaulting == true) {
+        e.player.setSwimming(true)
         e.player.setPose(Pose.SWIMMING)
         e.player.potionEffects.add('minecraft:speed', 1, 3, false, false)
+    } else {
+        e.player.setSwimming(false)
     }
 })
     
