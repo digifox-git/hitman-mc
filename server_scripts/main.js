9f7116ff3518c6fb068d5f01d54fef8592d64f7b
digifox <digifox08@gmail.com>
2026-08-14 17:26:22 -0500

    Update main.js

--- server_scripts/main.js
+++ server_scripts/main.js
@@ -492,6 +492,10 @@ PlayerEvents.tick(e => {
 
         // Check if near window, crouching, and at vent y level
         // If true, set isVaulting to true and set windowCoords to be used later
+        if (distance < 1.5) {
+            e.server.tell(`${Math.floor(e.player.y)} / ${Math.floor(vent.y)}`)
+        }
+
         if (distance < 1.5 && e.player.isCrouching() && Math.floor(e.player.y) == Math.floor(vent.y)) {
             windowCoords = {x: vent.x, y: vent.y, z: vent.z}
             isVaulting = true
