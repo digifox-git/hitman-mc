c141cb2ac1430ed3326df83a8c2099c121973a5d
digifox <digifox08@gmail.com>
2026-08-14 13:46:36 -0500

    Update main.js

--- server_scripts/main.js
+++ server_scripts/main.js
@@ -452,7 +452,7 @@ PlayerEvents.tick(e => {
             e.player.z - window.z
         )
 
-        if (distance < 3.5 && e.player.isCrouching()) {
+        if (distance < 3 && e.player.isCrouching()) {
             isVaulting = true
             break
         } else {
