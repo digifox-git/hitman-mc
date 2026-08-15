4d510806b3cbf4aa98c95e76329bdf465ce97163
digifox <digifox08@gmail.com>
2026-08-14 21:15:32 -0500

    Update main.js

--- server_scripts/main.js
+++ server_scripts/main.js
@@ -237,7 +237,7 @@ EntityEvents.death(e => {
 PlayerEvents.respawned(e => {
     let data = e.server.data;
     e.server.runCommandSilent(`gamemode spectator ${e.player.username}`)
-    e.server.runCommandSilent(`execute as ${e.player.username} run playsound minecraft:entity.allay.hurt player @a[gamemode=spectator,distance=0..2]`)
+    e.server.runCommandSilent(`execute at ${e.player.username} run playsound minecraft:entity.allay.hurt player @a[distance=0..2]`)
     if (e.player.tags.contains("guard")) {
         e.server.scheduleInTicks(120, () => {
             e.player.teleportTo(
