0346226138fe46b82780a2aafaa162adc59002fd
digifox <digifox08@gmail.com>
2026-08-14 21:21:12 -0500

    Update main.js

--- server_scripts/main.js
+++ server_scripts/main.js
@@ -238,6 +238,7 @@ PlayerEvents.respawned(e => {
     let data = e.server.data;
     e.server.runCommandSilent(`gamemode spectator ${e.player.username}`)
     e.server.runCommandSilent(`execute at ${e.player.username} run playsound minecraft:entity.allay.hurt player @a[distance=0..3] ~ ~ ~ 1 1`)
+    e.player.displayClientMessage(Component.white("You died! Respawning shortly..."), true);
     if (e.player.tags.contains("guard")) {
         e.server.scheduleInTicks(120, () => {
             e.player.teleportTo(
