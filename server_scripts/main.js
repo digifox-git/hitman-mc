3992b144a8ae2b85dfbd7832cbded39997f21dc2
digifox <digifox08@gmail.com>
2026-08-14 20:29:53 -0500

    Update main.js

--- server_scripts/main.js
+++ server_scripts/main.js
@@ -456,7 +456,7 @@ const Pose = Java.loadClass('net.minecraft.world.entity.Pose') // Load java clas
 PlayerEvents.tick(e => {
 
     // This code should only work for the hitman
-    if (!e.player.getTags().contains("hitman") || e.player.isSwimming()) {
+    if (!e.player.getTags().contains("hitman")) {
         return
     }
 
@@ -482,7 +482,7 @@ PlayerEvents.tick(e => {
 
         // Check if near window, crouching, and above window y level
         // If true, set isVaulting to true and set windowCoords to be used later
-        if (distance < 1.6 && e.player.isCrouching() && Math.floor(e.player.y) >= Math.floor(window.y)) {
+        if (distance < 1.6 && e.player.isCrouching() && Math.floor(e.player.y) >= Math.floor(window.y) + 1) {
             
             windowCoords = {x: window.x, y: window.y + 1, z: window.z}
             isVaulting = true
