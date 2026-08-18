13592d54ac8080e32084b6fbac42d7ed0e1d03c1
ThatCrazyManInASuit <skylarchung@gmail.com>
2026-08-18 17:52:59 -0500

    Update main.js

--- server_scripts/main.js
+++ server_scripts/main.js
@@ -536,6 +536,7 @@ PlayerEvents.tick(e => {
 EntityEvents.afterHurt(e => {
     if (e.getSource().getType() == "arrow") {
         e.entity.addTag("ragdoll")
+        e.server.tell("[DEBUG] KILL THIS MAN!")
         e.server.runCommandSilent(`ragdoll @a[tag=ragdoll] 100 0 ${e.entity.x} ${e.entity.y} ${e.entity.z}`)
         e.entity.removeTag("ragdoll")
     }
