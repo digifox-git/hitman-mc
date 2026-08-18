542514492635873ab8e5c0fbb1bc1f26937d119b
digifox <digifox08@gmail.com>
2026-08-18 18:27:13 -0500

    Update main.js

--- server_scripts/main.js
+++ server_scripts/main.js
@@ -75,7 +75,7 @@ ItemEvents.entityInteracted('minecraft:prismarine_crystals', e => {
     const nbt = entity.saveWithoutId()
 
     if (entity.type = "easy_npc:humanoid") {
-        player.tell("Yup")
+        e.server.runCommandSilent("say Yup")
     }
 })
 
